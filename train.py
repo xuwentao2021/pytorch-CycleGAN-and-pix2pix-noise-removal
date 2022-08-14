05386524415f2037f1e5941080b477c1ea2aeef6
theodoretsui <xu.wentao.2021@bristol.ac.uk>
2022-08-15 00:35:45 +0100

    Update train.py

--- train.py
+++ train.py
@@ -51,7 +51,7 @@ if __name__ == '__main__':
             model.set_input(data)         # unpack data from dataset and apply preprocessing
             model.optimize_parameters(epoch)   # calculate loss functions, get gradients, update network weights
 
-            if total_iters % opt.display_freq == 0 and epoch>opt.embedding_epochs:   # display images on visdom and save images to a HTML file
+            if total_iters % opt.display_freq == 0 and epoch>=opt.embedding_epochs:   # display images on visdom and save images to a HTML file
                 save_result = total_iters % opt.update_html_freq == 0
                 model.compute_visuals()
                 visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
