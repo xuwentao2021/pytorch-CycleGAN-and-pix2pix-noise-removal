01a519ea9f1c2d1b06a9812f88a5f29451148e77
xuwentao2021 <103892511+xuwentao2021@users.noreply.github.com>
2022-09-14 20:16:59 +0800

    Update download_moecyclegan_pretrain.sh

--- checkpoints/download_moecyclegan_pretrain.sh
+++ checkpoints/download_moecyclegan_pretrain.sh
@@ -1,8 +1,12 @@
 echo "Specified deepmoe_cyclegan_inference_eg"
-URL=https://drive.google.com/uc?export=download&id=1ITzTjiEWtuFNBahzeLPoNFiY7XSQCGKC
+
+ID=1ITzTjiEWtuFNBahzeLPoNFiY7XSQCGKC
 ZIP_FILE=./checkpoints/deepmoe_cyclegan_inference_eg.zip
 TARGET_DIR=./checkpoints/deepmoe_cyclegan_inference_eg/
 wget -N $URL -O $ZIP_FILE
+
+pip install gdown
+gdown -id $ID -O $ZIP_FILE
 mkdir $TARGET_DIR
 unzip $ZIP_FILE -d ./checkpoints/
 rm $ZIP_FILE
