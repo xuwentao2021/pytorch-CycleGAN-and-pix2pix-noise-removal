997e679941660427fa5d59ec97f9780dcba66ac3
xuwentao2021 <103892511+xuwentao2021@users.noreply.github.com>
2022-09-14 20:29:59 +0800

    Update download_cyclegan_pretrain.sh

--- checkpoints/download_cyclegan_pretrain.sh
+++ checkpoints/download_cyclegan_pretrain.sh
@@ -2,7 +2,6 @@ echo "Specified cyclegan_inference_eg"
 ID=1emmVAkbaShfDT3E6tubSIkMLld5FbWOT
 ZIP_FILE=./checkpoints/cyclegan_inference_eg.zip
 TARGET_DIR=./checkpoints/cyclegan_inference_eg/
-wget -N $URL -O $ZIP_FILE
 
 pip install gdown
 gdown -id $ID -O $ZIP_FILE
