d9e044e96d0ce4b823abe7fb1b1b384cc3bb0606
xuwentao2021 <103892511+xuwentao2021@users.noreply.github.com>
2022-09-14 20:15:02 +0800

    Update download_cyclegan_pretrain.sh

--- checkpoints/download_cyclegan_pretrain.sh
+++ checkpoints/download_cyclegan_pretrain.sh
@@ -1,8 +1,12 @@
-echo "Specified moe_cyclegan_inference_eg"
-URL=https://drive.google.com/uc?export=download&id=1emmVAkbaShfDT3E6tubSIkMLld5FbWOT
-ZIP_FILE=./checkpoints/moe_cyclegan_inference_eg.zip
-TARGET_DIR=./checkpoints/moe_cyclegan_inference_eg/
+echo "Specified cyclegan_inference_eg"
+ID=1emmVAkbaShfDT3E6tubSIkMLld5FbWOT
+ZIP_FILE=./checkpoints/cyclegan_inference_eg.zip
+TARGET_DIR=./checkpoints/cyclegan_inference_eg/
 wget -N $URL -O $ZIP_FILE
+
+pip install gdown
+gdown -id $ID -O $ZIP_FILE
 mkdir $TARGET_DIR
 unzip $ZIP_FILE -d ./checkpoints/
 rm $ZIP_FILE
+
